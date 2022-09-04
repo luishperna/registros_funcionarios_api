39f2ab278e85893784a29bbb3b0cfbacdab89563
Luís <luishperna@gmail.com>
2022-09-04 00:28:06 -0300

    Alterando informações da rota home

--- metadata/home_api.py
+++ metadata/home_api.py
@@ -5,8 +5,8 @@ informacoes_api = {
         'Usabilidade': 'Operações CRUD de registros de funcionários no banco de dados',
         'Rotas': {
         '/docs': 'Documentação gerada pelo FastApi',
-        '/': 'get - Home da API',
-        '/status': 'get - Status da API',
+        '/': 'Método GET - Home da API',
+        '/status': 'Método GET - Status da API',
         '/funcionarios/cadastros': 'Método POST - Cadastrar um registro',
         '/funcionarios/consultas/{cpf}': 'Método GET - Consultar um registro',
         '/funcionarios/modificacoes': 'Método PATCH - Modificar um registro',
