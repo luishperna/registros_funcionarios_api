1103870d190433e21cfba076ae7c59128dc0e0b0
Luís <luishperna@gmail.com>
2022-09-04 00:16:31 -0300

    Alterando informações da rota home

--- metadata/home_api.py
+++ metadata/home_api.py
@@ -2,16 +2,16 @@
 informacoes_api = {
         'Aplicação': 'Web/API',
         'Projeto': 'RegistrosFuncionariosApi',
-        'Usabilidade': 'Operações CRUD de funcionários no banco de dados',
+        'Usabilidade': 'Operações CRUD de registros de funcionários no banco de dados',
         'Rotas': {
         '/docs': 'Documentação gerada pelo FastApi',
-        '/api': 'get - Informações da API',
-        '/api/status': 'get - Status da API',
-        '/funcionarios/cadastros': 'post - Cadastrar informações de um funcionário',
-        '/funcionarios/consultas/{cpf}': 'get - Consultar informações de um funcionário',
-        '/funcionarios/modificacoes': 'patch - Modificar informações de um funcionário',
-        '/funcionarios/anulacoes/{cpf}': 'delete - Anular registro de um funcionário'
+        '/': 'get - Home da API',
+        '/status': 'get - Status da API',
+        '/funcionarios/cadastros': 'Método POST - Cadastrar um registro',
+        '/funcionarios/consultas/{cpf}': 'Método GET - Consultar um registro',
+        '/funcionarios/modificacoes': 'Método PATCH - Modificar um registro',
+        '/funcionarios/anulacoes/{cpf}': 'Método DELETE - Anular um registro'
         },
         'Finalidade': 'Simulação/Estudo',
-        'Desenvolvedor': 'Luís Perna'
+        'Desenvolvedor': 'Luís Henrique Perna'
     }
